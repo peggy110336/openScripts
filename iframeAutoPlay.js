4f9f0b8bb573fa9f1a22ef5a134a3996c0ef8b1a
K - Peggy <peggy110336@gmail.com>
2019-07-08 05:46:12 +0800

    add jquery link

--- iframeAutoPlay.js
+++ iframeAutoPlay.js
@@ -1,4 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
+   
 $(document).ready(function(){
    function html(img) {
      var iframe = document.getElementById("frame");
